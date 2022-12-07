4641685c18042c501355ca915db58fdb4c1a1279
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-12-07 20:20:25 +0600

    another commit

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -7,23 +7,27 @@ const DashboardLayout = () => {
     <div>
       <Navbar></Navbar>
       <div className="drawer drawer-mobile bg-slate-400">
-        <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+        <input
+          id="dashboard-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+        />
         <div className="drawer-content flex flex-col items-center">
-        <Outlet></Outlet>
+          <Outlet></Outlet>
         </div>
         <div className="drawer-side">
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100 text-base-content">
             <li>
-              <Link>Sidebar Item 1</Link>
+              <Link to="/dashboard/allbuyers">All Buyers</Link>
             </li>
             <li>
-              <Link>Sidebar Item 2</Link>
+              <Link to="/dashboard/allsellers">All Sellers</Link>
             </li>
+            
           </ul>
         </div>
       </div>
-      
     </div>
   );
 };
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="text-3xl my-5 text-white text-center">Dashboard</h3>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
